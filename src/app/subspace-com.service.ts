0f95b9c75d096c16c5f501972c31772a088de566
Mike Gont <mike.gont@gmail.com>
2020-11-28 21:51:52 +0200

    started working on the communication layer: SubspaceComService

---
+++ src/app/subspace-com.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SubspaceComService {
+
+
+
+  constructor() { }
+
+  sendMessage(targetId: string, payload: string): void {
+  }
+}
